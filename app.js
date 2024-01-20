ea998208076ccd1a34e27980bf263a4a998af7f5
Ismailazadkhan <144444823+Ismailazadkhan@users.noreply.github.com>
2024-01-20 17:38:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-var inputval = document.querySelector('#cityinput')
-var btn = document.querySelector('#add');
-var city = document.querySelector('#cityoutput')
-var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
-var wind = document.querySelector('#wind')
-apik = "4b3d324d8f4b9d9afd39e985e8864db1"
-function convertion(val)
-{
-    return (val - 273).toFixed(2)
-}
+document.getElementById('add').addEventListener('click', function () {
+ const city = document.getElementById('cityinput').value;
 
-btn.addEventListener('click', function()
-{
-  fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key})
-  .then(res => res.json())
-
-
-  .then(data => 
-  {
-    var nameval = data['name']
-    var descrip = data['weather']['0']['description']
-    var tempature = data['main']['temp']
-    var wndspd = data['wind']['speed']
-    city.innerHTML=`Weather of <span>${nameval}<span>`
-    temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
-    description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
-    wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
-
-  })
-
-  .catch(err => alert('You entered Wrong city name'))
-})
+ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b3d324d8f4b9d9afd39e985e8864db1`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('cityoutput').textContent = data.name;
+      document.getElementById('description').textContent = data.weather[0].description;
+      document.getElementById('temp').textContent = Math.round(data.main.temp - 273.15) + '°C';
+      document.getElementById('wind').textContent = Math.round(data.wind.speed) + 'm/s';
+    });
+});
