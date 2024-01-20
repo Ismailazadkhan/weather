3a38a398e0b2b483e3f245e32f8a89c862df3729
Ismailazadkhan <144444823+Ismailazadkhan@users.noreply.github.com>
2024-01-20 17:22:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function convertion(val)
 
 btn.addEventListener('click', function()
 {
-  fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
+  fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key})
   .then(res => res.json())
 
 
