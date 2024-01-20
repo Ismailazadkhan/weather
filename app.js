91525a13de267615016b24a56a4b87ffa9b10c6b
Ismailazadkhan <144444823+Ismailazadkhan@users.noreply.github.com>
2024-01-20 17:17:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-const apiKey = '4b3d324d8f4b9d9afd39e985e8864db1';
-const apiUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
-const apiIconUrl = 'https://openweathermap.org/img/wn';
-
-const locationInput = document.getElementById('locationInput');
-const searchButton = document.getElementById('searchButton');
-const locationElement = document.getElementById('location');
-const temperatureElement = document.getElementById('temperature');
-const descriptionElement = document.getElementById('description');
-const windSpeedElement = document.getElementById('windSpeed');
-const weatherIconElement = document.getElementById('weatherIcon');
-
-searchButton.addEventListener('click', () => {
-    const location = locationInput.value;
-    if (location) {
-        fetchWeather(location);
-    }
-});
-
-function fetchWeather(location) {
-    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            locationElement.textContent = data.name;
-            temperatureElement.textContent = `${data.main.temp}°C`;
-            descriptionElement.textContent = data.weather[0].description;
-            windSpeedElement.textContent = `${data.wind.speed} m/s`;
-            weatherIconElement.src = `${apiIconUrl}/${data.weather[0].icon}.png`;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik = "4b3d324d8f4b9d9afd39e985e8864db1"
+function convertion(val)
+{
+    return (val - 273).toFixed(2)
 }
+
+btn.addEventListener('click', function()
+{
+  fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
+  .then(res => res.json())
+
+
+  .then(data => 
+  {
+    var nameval = data['name']
+    var descrip = data['weather']['0']['description']
+    var tempature = data['main']['temp']
+    var wndspd = data['wind']['speed']
+    city.innerHTML=`Weather of <span>${nameval}<span>`
+    temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+    description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+    wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
+
+  })
+
+  .catch(err => alert('You entered Wrong city name'))
+})
