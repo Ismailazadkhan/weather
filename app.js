524bf74c6491f7b53b5aa2002b6696b07fd20adb
Ismail azad khan <144444823+Ismailazadkhan@users.noreply.github.com>
2024-01-25 20:32:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ document.getElementById('add').addEventListener('click', function () {
       document.getElementById('description').textContent = data.weather[0].description;
       document.getElementById('temp').textContent = Math.round(data.main.temp - 273.15) + '°C';
       document.getElementById('wind').textContent = Math.round(data.wind.speed) + 'm/s';
+
+     searchButton.addEventListener("click", () => {
+  const city = cityInput.value;
+  getWeatherData(city);
+});
+
+cityInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    const city = cityInput.value;
+    getWeatherData(city);
       
     });
 });
